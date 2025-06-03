57d4d7687c1aeebe9b5fd1fbe5929b758e6795b0
Khagesh Nemade <khageshnemade@gmail.com>
2025-06-03 22:45:38 +0530

    Addhealthcheck

--- src/main/java/com/chatapp/realchat/config/HelloController.java
+++ src/main/java/com/chatapp/realchat/config/HelloController.java
@@ -17,13 +17,13 @@ public class HelloController {
     
     
     // 2. Welcome user
-    @GetMapping("/welcome")
+    @GetMapping("/welcome1")
     public Map<String, String> welcomeUser() {
         return Map.of("status", "success", "message", "Welcome to the Chat App!");
     }
 
     // 3. App info
-    @GetMapping("/info")
+    @GetMapping("/info1")
     public Map<String, String> getAppInfo() {
         return Map.of(
             "app", "RealChat",
